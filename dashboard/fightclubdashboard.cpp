82b35cfd1520288a6a9a704946ab34a02363a85e
SFr682k <sfr682k@t-online.de>
2018-04-30 13:41:57 +0200

    Implement basic dashboard window behavior

--- dashboard/fightclubdashboard.cpp
+++ dashboard/fightclubdashboard.cpp
@@ -21,6 +21,8 @@
 
 #include "aboutdialog.h"
 
+#include <QTime>
+
 
 FightclubDashboard::FightclubDashboard(QWidget *parent) :
     QMainWindow(parent),
@@ -28,11 +30,16 @@ FightclubDashboard::FightclubDashboard(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    refreshtimer = new QTimer();
+    refreshtimer->start(30);
+
+    connect(refreshtimer, SIGNAL(timeout()), this, SLOT(updateTimeDisplay()));
+
     aboutDialogOpen = false;
 }
 
-FightclubDashboard::~FightclubDashboard()
-{
+
+FightclubDashboard::~FightclubDashboard() {
     delete ui;
 }
 
@@ -43,5 +50,64 @@ void FightclubDashboard::openAboutDialog() {
         AboutDialog *ad = new AboutDialog(this);
         ad->exec();
         aboutDialogOpen = false;
+    }
+}
+
+
+
+
+void FightclubDashboard::updateTimeDisplay() {
+    QString currentTime = QTime::currentTime().toString("HH:mm");
+    if(QTime::currentTime().second() % 2 != 0) currentTime[2] = ' ';
+    ui->currenttime->display(currentTime);
+}
+
+
+
+void FightclubDashboard::resizeEvent(QResizeEvent *event) {
+    QFont tnamefont = font();
+    tnamefont.setBold(true);
+    tnamefont.setPointSize(height()*0.026);
+    ui->tournamentname->setFont(tnamefont);
+
+    /*
+    QFont phaselabelfont = ui->phaselabel->font();
+    phaselabelfont.setPointSize(2 + height()*0.03);
+
+    QFont infolabelfont = ui->perflabel->font();
+    infolabelfont.setPointSize(4 + height()*0.015);
+
+    ui->phaselabel->setFont(phaselabelfont);
+    ui->problabel->setFont(infolabelfont);
+    ui->perflabel->setFont(infolabelfont);
+    */
+
+    QWidget::resizeEvent(event);
+}
+
+
+
+void FightclubDashboard::keyPressEvent(QKeyEvent *event) {
+    switch(event->key()) {
+        case Qt::Key_F1:
+            openAboutDialog();
+            break;
+
+        case Qt::Key_F:
+            if(QApplication::keyboardModifiers() & Qt::ControlModifier)
+                setWindowState(Qt::WindowFullScreen);
+            break;
+
+        case Qt::Key_Escape:
+            if(windowState() == Qt::WindowFullScreen) setWindowState(Qt::WindowMaximized);
+            break;
+
+        case Qt::Key_Q:
+            if(QApplication::keyboardModifiers() & Qt::ControlModifier)
+                this->close();
+            break;
+
+        default:
+            QWidget::keyPressEvent(event);
     }
 }
--- dashboard/fightclubdashboard.h
+++ dashboard/fightclubdashboard.h
@@ -21,6 +21,10 @@
 
 #include <QMainWindow>
 
+#include <QKeyEvent>
+#include <QTimer>
+
+
 namespace Ui {
 class FightclubDashboard;
 }
@@ -37,6 +41,15 @@ public:
 private:
     Ui::FightclubDashboard *ui;
     bool aboutDialogOpen;
+    QTimer *refreshtimer, *switchpagetimer;
+
+protected:
+    void resizeEvent(QResizeEvent *event) override;
+    void keyPressEvent(QKeyEvent *event) override;
+
+
+private slots:
+    void updateTimeDisplay();
 };
 
 #endif // FIGHTCLUBDASHBOARD_H
